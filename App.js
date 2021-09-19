e56fa7865f957eeb7ce8ac48e64c41f4c18bd812
Sandaka <sandaka94@gmail.com>
2021-09-19 14:44:24 +0530

    login page design

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,6 +16,10 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -26,87 +30,130 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.sectionContainer}>
+      <Image source={require('./assets/images/different-bakery-products-on-white-background-vector-30927396.jpg')} style={{ width: 391, height: 300, alignItems: 'center' }} />
+      <Text style={styles.title}>The Bake Vibes</Text>
+
+      <View style={styles.loginSection}>
+
+        {/* <Text style={styles.loginText}>Login</Text> */}
+
+        <TextInput style={styles.textFields} placeholder='Username'></TextInput>
+        <TextInput style={styles.textFields} placeholder='Password'></TextInput>
+
+        <View style={styles.loginButton}>
+          <Button title='Login' color='orange'></Button>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        {/* <TouchableOpacity style={styles.loginButton}>
+          <Text>Button Text</Text>
+        </TouchableOpacity> */}
+      </View>
+
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    //backgroundColor: '#000000',
+    //height: '100%',
+
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  title: {
+    color: 'orange',
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'Calibri',
+    textAlign: 'center',
+    marginTop: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  loginText: {
+    color: 'grey',
+    marginTop: 20,
+    fontSize: 20,
+    textAlign: 'center'
   },
-  highlight: {
-    fontWeight: '700',
+
+  textFields: {
+    // borderColor: '#7a42f4',
+    height: 50,
+    //borderStyle: 'solid',
+    color: 'black',
+    marginTop: 20,
+    borderRadius: 1,
+    elevation: 1,
+    backgroundColor: 'white',
+    width: 300
   },
+
+  loginSection: {
+    //justifyContent: 'center',
+    //flex: 1,
+    alignItems: 'center'
+  },
+
+  loginButton: {
+    marginTop: 30,
+    backgroundColor: 'orange',
+    width: 100,
+    borderRadius: 10,
+  }
 });
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
 
 export default App;
